78ecda00f218c3caf5596d77d6032e42a54f0c5f
godcong <jumbycc@163.com>
2024-12-13 16:32:33 +0800

    feat(settings): add support for error handling in default settings
    - Introduce ErrorDefaulter interface for types that provide default settings with error handling
    - Implement ApplyErrorDefaults function to apply settings and handle errors
    - Add convenience wrappers for ApplyErrorDefaults
    - Extend existing ApplyDefaults functions to support error handling

--- settings/default.go
+++ settings/default.go
@@ -7,6 +7,12 @@ type Defaulter interface {
 	ApplyDefaults()
 }
 
+// ErrorDefaulter is an interface that provides a method to apply default settings and return an error.
+type ErrorDefaulter interface {
+	// ApplyDefaults applies the default settings to the implementing type and returns an error.
+	ApplyDefaults() error
+}
+
 // ApplyDefaults applies the given settings and default settings to the provided value.
 //
 // It first applies the given settings using the Apply function, then checks if the value
@@ -20,6 +26,21 @@ func ApplyDefaults[S any](s *S, fs []func(*S)) *S {
 	return s
 }
 
+// ApplyErrorDefaults applies the given settings and default settings to the provided value.
+//
+// It first applies the given settings using the Apply function, then checks if the value
+// implements the ErrorDefaulter interface. If it does, it calls the ApplyDefaults method
+func ApplyErrorDefaults[S any](s *S, fs []func(*S)) (*S, error) {
+	s = Apply(s, fs)
+	if d, ok := any(s).(ErrorDefaulter); ok {
+		err := d.ApplyDefaults()
+		if err != nil {
+			return s, err
+		}
+	}
+	return s, nil
+}
+
 // ApplyDefaultsOr applies the given settings and default settings to the provided value.
 //
 // It is a convenience wrapper around ApplyDefaults that accepts a variable number of setting functions.
@@ -28,10 +49,33 @@ func ApplyDefaultsOr[S any](s *S, fs ...func(*S)) *S {
 	return ApplyDefaults(s, fs)
 }
 
+// ApplyErrorDefaultsOr applies the given settings and default settings to the provided value.
+//
+// It is a convenience wrapper around ApplyDefaults that accepts a variable number of interface{} values.
+func ApplyErrorDefaultsOr[S any](s *S, fs ...func(*S)) (*S, error) {
+	return ApplyErrorDefaults(s, fs)
+}
+
 // ApplyDefaultsOrZero applies the given settings and default settings to a zero value of the type.
 //
 // It creates a zero value of the type, then calls ApplyDefaults to apply the given settings and default settings.
 func ApplyDefaultsOrZero[S any](fs ...func(*S)) *S {
 	var zero S
 	return ApplyDefaults(&zero, fs)
+}
+
+// ApplyErrorDefaultsOrZero applies the given settings and default settings to a zero value of the type.
+//
+// It creates a zero value of the type, then calls ApplyDefaults to apply the given settings and default settings.
+func ApplyErrorDefaultsOrZero[S any](fs ...func(*S)) (*S, error) {
+	var zero S
+	return ApplyErrorDefaults(&zero, fs)
+}
+
+// ApplyDefaultsOrError applies the given settings and default settings to the provided value.
+//
+// It is a convenience wrapper around ApplyDefaults that accepts a variable number of setting functions.
+func ApplyDefaultsOrError[S any](s *S, fs ...func(*S)) (*S, error) {
+	// Call ApplyDefaults with the provided value and setting functions
+	return ApplyErrorDefaults(s, fs)
 }
