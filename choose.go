c4b97b283047425b65e0cc68610d17966de9b30d
godcong <jumbycc@163.com>
2025-04-25 18:30:13 +0800

    refactor(choose): update type constraints for generic functions
    - Update type constraints for ChooseRight, ChooseLeft, OnlyLeft, OnlyRight, and OnlyMiddle functions
    - Change [L any, R any] to [L, R any] for better readability and consistency
    - This change improves the syntax uniformity and enhances code maintainability

--- choose.go
+++ choose.go
@@ -25,30 +25,30 @@ func Choose[T any](cond bool, l, r T) T {
 
 // ChooseRight returns the rightmost value, ignoring the left value.
 // This function is useful when you need to prioritize the right value over the left.
-func ChooseRight[L any, R any](_ L, r R) R {
+func ChooseRight[L, R any](_ L, r R) R {
 	return r
 }
 
 // ChooseLeft returns the leftmost value, ignoring the right value.
 // This function is useful when you need to prioritize the left value over the right.
-func ChooseLeft[L any, R any](l L, _ R) L {
+func ChooseLeft[L, R any](l L, _ R) L {
 	return l
 }
 
 // OnlyLeft returns the leftmost value, ignoring all other values.
 // This function is useful when you need to extract the leftmost value from a tuple.
-func OnlyLeft[L any, R1 any, R2 any](l L, _ R1, _ R2) L {
+func OnlyLeft[L, R1, R2 any](l L, _ R1, _ R2) L {
 	return l
 }
 
 // OnlyRight returns the rightmost value, ignoring all other values.
 // This function is useful when you need to extract the rightmost value from a tuple.
-func OnlyRight[L1 any, L2 any, R any](_ L1, _ L2, r R) R {
+func OnlyRight[L1, L2, R any](_ L1, _ L2, r R) R {
 	return r
 }
 
 // OnlyMiddle returns the middle value, ignoring all other values.
 // This function is useful when you need to extract the middle value from a tuple.
-func OnlyMiddle[L any, M any, R any](_ L, m M, _ R) M {
+func OnlyMiddle[L, M, R any](_ L, m M, _ R) M {
 	return m
 }
